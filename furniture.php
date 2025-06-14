8b5e0f7c126fbfc24038e2ef6a121edf911040d2
oweeeeens <paulowen.havana@cvsu.edu.ph>
2025-06-14 19:38:28 +0800

    Categories Catalog Page

---
+++ furniture.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>DigitF</title>
+
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700&display=swap" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet" />
+  <link href="css/responsive.css" rel="stylesheet" />
+  <link href="images/logo.png" rel="icon" type="image/svg+mgl">
+</head>
+
+<body class="sub_page">
+
+<?php include 'index-header.php' ?>
+
+  <!-- trending section -->
+
+  <section class="trending_section layout_padding">
+    <div id="accordion">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="detail-box">
+              <div class="heading_container">
+                <h2>
+                  Trending Categories
+                </h2>
+              </div>
+              <div class="tab_container">
+                <div class="t-link-box" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                  aria-controls="collapseOne">
+                  <div class="number">
+                    <h5>
+                      01
+                    </h5>
+                  </div>
+                  <hr>
+                  <div class="t-name">
+                    <h5>
+                      Chairs
+                    </h5>
+                  </div>
+                </div>
+                <div class="t-link-box collapsed" data-toggle="collapse" data-target="#collapseTwo"
+                  aria-expanded="false" aria-controls="collapseTwo">
+                  <div class="number">
+                    <h5>
+                      02
+                    </h5>
+                  </div>
+                  <hr>
+                  <div class="t-name">
+                    <h5>
+                      Tables
+                    </h5>
+                  </div>
+                </div>
+                <div class="t-link-box collapsed" data-toggle="collapse" data-target="#collapseThree"
+                  aria-expanded="false" aria-controls="collapseThree">
+                  <div class="number">
+                    <h5>
+                      03
+                    </h5>
+                  </div>
+                  <hr>
+                  <div class="t-name">
+                    <h5>
+                      Bads
+                    </h5>
+                  </div>
+                </div>
+                <div class="t-link-box collapsed" data-toggle="collapse" data-target="#collapseFour"
+                  aria-expanded="false" aria-controls="collapseFour">
+                  <div class="number">
+                    <h5>
+                      04
+                    </h5>
+                  </div>
+                  <hr>
+                  <div class="t-name">
+                    <h5>
+                      Furnitures
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="collapse show" id="collapseOne" aria-labelledby="headingOne" data-parent="#accordion">
+              <div class="img_container ">
+                <div class="box b-1">
+                  <div class="img-box">
+                    <img src="images/t-1.png" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-2.jpg" alt="">
+                  </div>
+                </div>
+                <div class="box b-2">
+                  <div class="img-box">
+                    <img src="images/t-3.jpg" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-4.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="collapse" id="collapseTwo" aria-labelledby="headingTwo" data-parent="#accordion">
+              <div class="img_container ">
+                <div class="box b-1">
+                  <div class="img-box">
+                    <img src="images/t-3.jpg" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-4.png" alt="">
+                  </div>
+                </div>
+                <div class="box b-2">
+
+                  <div class="img-box">
+                    <img src="images/t-1.png" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-2.jpg" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="collapse" id="collapseThree" aria-labelledby="headingThree" data-parent="#accordion">
+              <div class="img_container ">
+                <div class="box b-1">
+                  <div class="img-box">
+                    <img src="images/t-4.png" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-1.png" alt="">
+                  </div>
+                </div>
+                <div class="box b-2">
+                  <div class="img-box">
+                    <img src="images/t-3.jpg" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-2.jpg" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="collapse" id="collapseFour" aria-labelledby="headingfour" data-parent="#accordion">
+              <div class="img_container ">
+                <div class="box b-1">
+                  <div class="img-box">
+                    <img src="images/t-1.png" alt="">
+                  </div>
+
+                  <div class="img-box">
+                    <img src="images/t-4.png" alt="">
+                  </div>
+                </div>
+                <div class="box b-2">
+                  <div class="img-box">
+                    <img src="images/t-3.jpg" alt="">
+                  </div>
+                  <div class="img-box">
+                    <img src="images/t-2.jpg" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <!-- end trending section -->
+<?php include 'index-footer.php'  ?>
+
+</body>
+
+</html>
